35a496aa4868d872de42fb709d283db1b4befec6
Trieu Thanh Dat <dat.tt@amanotes.com>
2024-04-06 15:24:12 +0700

    [RE-WORK] - rework Lobby

---
+++ Assets/_MODULE/TD/UServices/Lobbies/Messages/LobbyListFetchedMessage.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace TD.UServices.Lobbies
+{
+    public struct LobbyListFetchedMessage
+    {
+        public readonly IReadOnlyList<LocalLobby> LocalLobbies;
+
+        public LobbyListFetchedMessage(List<LocalLobby> localLobbies)
+        {
+            LocalLobbies = localLobbies;
+        }
+    }
+}
